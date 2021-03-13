ed946122c7321a0a1fc3969ace57e24b00b3edde
nebverse <75533470+nebverse@users.noreply.github.com>
2021-03-13 16:59:58 +0530

    Delete p1.py

--- p1.py
+++
@@ -1,6 +0,0 @@
-from bing_image_downloader import downloader
-
-downloader.download("aamir khan",limit=200,output_dir="photo_trial", adult_filter_off=True)
-downloader.download("hrithik roshan",limit=200,output_dir="photo_trial", adult_filter_off=True)
-downloader.download("shahrukh khan",limit=200,output_dir="photo_trial", adult_filter_off=True)
-downloader.download("ayushmann khurrana",limit=200,output_dir="photo_trial", adult_filter_off=True)
